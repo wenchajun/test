1adf0e4cdcd33a95943083b7303ad4ce08aad6ca
wenchajun <995729579@qq.com>
2021-05-25 14:14:17 +0800

    A printed log with a running number

--- test.go
+++ test.go
@@ -25,14 +25,15 @@ func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
 }
 
-var N = flag.IntP("cycles", "N", 10, "Cycles per second")
+var N = flag.IntP("cycles", "N", 4, "Cycles per second")
 var M = flag.IntP("logSize", "M", 2, "Output log size")
 var serial = 0 //流水号
 var lock sync.Mutex
+//var quit chan int
 
 func main() {
 	flag.Parse()
-
+	var c=make(chan int,*N)
 	for {
 		fmt.Println(*N)
 		fmt.Println(*M)
@@ -41,21 +42,29 @@ func main() {
 
 		//如果每秒发送2000条内就不用开启多线程
 		//超过2000条，开启n/2000个线程/协程
-		if *N < 2000 {
+		if *N < 1000 {
 			Log(*N, *M)
 		} else {
-			threadN := (*N) / 20000
-			for i := 0; i < threadN; i++ {
-				go Log(*N, *M)
+			threadN := (*N) / 500
+
+			for i :=0;i<*N;i++{
+				serial++
+				c <- serial
 			}
-		}
+			for i := 0; i < threadN; i++ {
+				//go concurrencylog(cap(c),c,*M)
+				go concurrencylog(c,*M)
+					}
+
+				}
 		end := time.Now().UnixNano()
 		elapsed := end - start
 		//elapsed := time.Since(t1).Nanoseconds()//计算过去了多久的时间
 
+		//fmt.Println("-------------")
+		//fmt.Println(elapsed)
 		fmt.Println("-------------")
 		fmt.Println(elapsed)
-		fmt.Println("-------------")
 
 		if elapsed < 1000000000 {
 			time.Sleep(time.Duration(1000000000-elapsed) * time.Nanosecond)
@@ -73,12 +82,12 @@ func Log(cycles int, logSize int) {
 			//date := now.Format("2006-01-02 15:04:05")
 			//userId := rand.Intn(10000)
 
-			lock.Lock()
+			//lock.Lock()
 			serial = serial + 1
 			//log.Println("----------"+ "--------------",serial)
 			//lock.Unlock()
 
-			result := "elon|"  + "|" + a + "|" +"本次流水号是"+ strconv.Itoa(serial)
+			result := "name:eloncheng|"  + "|" + a + "|" +"The current number is"+ strconv.Itoa(serial)
 
 			for m := 0; m < logSize; m++ {
 				result = result + result
@@ -86,7 +95,48 @@ func Log(cycles int, logSize int) {
 			//fmt.Println(result)
             //fmt.Println("----------"+ "--------------",serial)
 			log.Println(result) // log 还是可以作为输出的前缀
-			lock.Unlock()
+			fmt.Println(result)
+			//lock.Unlock()
+
 		}()
 	}
+}
+
+//func concurrencylog(N int,c chan int,logSize int){
+//	for i := 0; i < N; i++ {
+//		a := "fluent-bit-test"
+//		serial = serial + 1
+//		c <-serial  // 发消息：我执行完啦！
+//		result := "name:eloncheng|" + "|" + a + "|" + "The current number is" + strconv.Itoa(serial)
+//
+//		for m := 0; m < logSize; m++ {
+//			result = result + result
+//		}
+//
+//		log.Println(result) // log 还是可以作为输出的前缀
+//		fmt.Println(result)
+//
+//
+//	}
+//	close(c)
+//}
+func concurrencylog(c chan int,logSize int){
+	for  {
+		lock.Lock()
+		No, ok := <-c
+		if !ok{
+			break
+		}
+		a := "fluent-bit-test"
+		// 发消息：我执行完啦！
+		result := "name:eloncheng|" + "|" + a + "|" + "The current number is" + strconv.Itoa(No)
+		for m := 0; m < logSize; m++ {
+			result = result + result
+		}
+
+		fmt.Println(result)
+		lock.Unlock()
+
+	}
+	//close(c)
 }
